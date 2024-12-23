23775d707fb1f3dcdf21a874cf4e062aeb866772
FairuzLQ <elqimochammad12@gmail.com>
2024-12-23 20:42:21 +0700

    responsive footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,9 +1,9 @@
 export default function Footer() {
     return (
-      <footer className="bg-white text-black pt-8 pb-20 px-12 ml-10">
+      <footer className="bg-white text-black pt-8 pb-20 px-12">
         <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* First Column (Logo and Slogan) */}
-          <div className="col-span-1 md:col-span-1 flex flex-col px-4">
+          <div className="col-span-1 md:col-span-1 flex flex-col px-4 sm:ml-0 md:ml-10">
             {/* Logo */}
             <div className="mb-4">
               <img src="/logo/logo-kuno-footer.png" alt="Kupas Tekno Logo" className="w-[200px]" />
@@ -16,7 +16,7 @@ export default function Footer() {
           </div>
   
           {/* Second Column (Beranda, How To, What Is) */}
-          <div className="col-span-1 md:col-span-1 px-4">
+          <div className="col-span-1 md:col-span-1 px-4 sm:ml-0 md:ml-10">
             <ul className="space-y-2">
               <li><a href="/" className="text-black">Beranda</a></li>
               <li><a href="/kategori/how-to" className="text-black">How To</a></li>
@@ -25,7 +25,7 @@ export default function Footer() {
           </div>
   
           {/* Third Column (Overview, Tech News, Sitemap) */}
-          <div className="col-span-1 md:col-span-1 px-4">
+          <div className="col-span-1 md:col-span-1 px-4 sm:ml-0 md:ml-10">
             <ul className="space-y-2">
               <li><a href="/kategori/overview" className="text-black">Overview</a></li>
               <li><a href="/kategori/tech-news" className="text-black">Tech News</a></li>
@@ -34,7 +34,7 @@ export default function Footer() {
           </div>
   
           {/* Fourth Column (Syarat dan Ketentuan, Kebijakan Privasi, Kontak) */}
-          <div className="col-span-1 md:col-span-1 px-4">
+          <div className="col-span-1 md:col-span-1 px-4 sm:ml-0 md:ml-10">
             <ul className="space-y-2">
               <li><a href="/syarat-ketentuan" className="text-black">Syarat dan Ketentuan</a></li>
               <li><a href="/kebijakan-privasi" className="text-black">Kebijakan Privasi</a></li>
